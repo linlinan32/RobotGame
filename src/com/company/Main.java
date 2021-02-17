0a86e98c9b32213410d53e2538e493e7ff390b63
linlinan32 <54700872+lln0034@users.noreply.github.com>
2021-02-17 23:06:01 +1100

    classified turn around calculate method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,18 +16,114 @@ public class Main {
                 "* `R1` - turn right 90 degrees\n" +
                 "* `L1` - turn left 90 degrees");
 
-        String move=n.nextLine();
-        String array[] = inputValidate(move);
-        System.out.println(array[0]+array[1]);
+        String move=n.nextLine(); //take user input
+        String array[] = inputValidate(move);//split user input by common and trim the spaces
 
-        int l = move.length();
-        int countUp = 0, countDown = 0;
-        int countLeft = 0, countRight = 0;
+
+        int moveUp = 0, moveDown = 0;
+        int trunLeft = 1, trunRight = 1;
+        //assume the movement is located in the x-axis and y-axis movement
+        //indicate the movement by (x,y)
+        //indicate every turn around by L or R, the quadrant would change also
+
+        for(int i=0; i<array.length;i++){
+            System.out.println(inputMovement(array[i])[1]);
+
+
+            //here is to calculate the x and y position by taking user input
+        }
 
     }
     public static String[] inputValidate(String moveInput){
 
-        String array[]=moveInput.split(",");
+        String array[]=moveInput.trim().split(",");
         return array;
+    }
+    public static Integer[] inputMovement(String movement)
+    {
+        Integer x[]={0,0};
+
+        if(movement.contains("L") ||movement.contains("l") ){
+           Integer direction= Integer.parseInt(
+                    movement.substring(1)) % 4;
+
+            return inputTurn(direction);
+        }
+        if(movement.contains("R") ||movement.contains("r") ){
+            Integer direction=  Integer.parseInt(
+                    movement.substring(1)) % 4;
+
+            return inputTurnRight(direction);
+        }
+
+
+        return x;
+
+    }
+
+    /**
+     *
+     * @param direction
+     * this method give input of no of turn left
+     * @return
+     */
+    public static Integer[] inputTurn(Integer direction){
+
+        Integer x[]={0,0};
+        //turn left
+        if(direction==1){
+             x[0]=-1;
+            x[1]=1;//turn once from quadrant 1 to 2
+            return x;
+        }
+        if(direction==2){
+            x[0]=-1;
+            x[1]=-1;//turn once from quadrant 2 to 3
+            return x;
+        }
+        if(direction==3){
+            x[1]=-1;
+            x[0]=1;//turn once from quadrant 3 to 4
+            return x;
+        }
+        if(direction==4){
+            x[0]=1;
+            x[1]=1;//turn once from quadrant 4 to 1
+            return x;
+        }
+        return x;
+    }
+
+    /**
+     *
+     * @param direction
+     * this method give the input of no of turn right
+     * @return
+     */
+    public static Integer[] inputTurnRight(Integer direction){
+
+        Integer x[]={0,0};
+        //turn right
+        if(direction==1){
+            x[1]=-1;
+            x[0]=1;//turn once from quadrant 1 to 4
+            return x;
+        }
+        if(direction==2){
+            x[0]=-1;
+            x[1]=-1;//turn once from quadrant 4 to 3
+            return x;
+        }
+        if(direction==3){
+            x[0]=-1;
+            x[1]=1;//turn once from quadrant 3 to 2
+            return x;
+        }
+        if(direction==4){
+            x[0]=1;
+            x[1]=1;//turn once from quadrant 2 to 1
+            return x;
+        }
+        return x;
     }
 }
